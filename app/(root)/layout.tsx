0b954923cfdb32dab92a20a41ccea56dbe3484df
Alan Francis <alan.prime567@gmail.com>
2024-10-01 21:02:38 +0530

    chor:scrolltrigerupdate

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { ReactLenis } from "lenis/react";
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "@/components/ui/footer";
 import Nav from "@/components/ui/nav";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.refresh();
+  }, []);
   return (
     <ReactLenis root className="relative">
       <Nav />
