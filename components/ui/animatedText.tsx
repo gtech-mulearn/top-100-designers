6f148eba409d224154ab00714bed74d6d6696608
Alan Francis <alan.prime567@gmail.com>
2024-10-02 12:49:16 +0530

    feat:add accordion

--- components/ui/animatedText.tsx
+++ components/ui/animatedText.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion, useScroll, useTransform, MotionValue } from "framer-motion";
+import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
 import React, { useRef } from "react";
 
 interface ParagraphProps {
@@ -12,20 +12,24 @@ interface WordProps {
   range: [number, number];
 }
 
+interface CharProps {
+  children: string;
+  progress: MotionValue<number>;
+  range: [number, number];
+}
+
 export default function Paragraph({ paragraph }: ParagraphProps) {
   const container = useRef<HTMLParagraphElement>(null);
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start 0.9", "start 0.45"],
+    offset: ["start 0.9", "start 0.25"],
   });
-
   const words = paragraph.split(" ");
   return (
-    <p ref={container} className="flex flex-wrap leading-none text-white">
+    <p ref={container} className="flex flex-wrap leading-none  text-white">
       {words.map((word, i) => {
         const start = i / words.length;
         const end = start + 1 / words.length;
-        // console.log(start, end);
         return (
           <Word key={i} progress={scrollYProgress} range={[start, end]}>
             {word}
@@ -37,9 +41,27 @@ export default function Paragraph({ paragraph }: ParagraphProps) {
 }
 
 const Word = ({ children, progress, range }: WordProps) => {
-  const opacity = useTransform(progress, range, [0, 1]);
+  const amount = range[1] - range[0];
+  const step = amount / children.length;
   return (
     <span className="relative mr-2 mt-2">
+      {children.split("").map((char, i) => {
+        const start = range[0] + i * step;
+        const end = range[0] + (i + 1) * step;
+        return (
+          <Char key={`c_${i}`} progress={progress} range={[start, end]}>
+            {char}
+          </Char>
+        );
+      })}
+    </span>
+  );
+};
+
+const Char = ({ children, progress, range }: CharProps) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <span className="relative">
       <span className="absolute opacity-20">{children}</span>
       <motion.span style={{ opacity }}>{children}</motion.span>
     </span>
--- components/ui/faq.tsx
+++ components/ui/faq.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import AnimatedPara from "./animatedPara";
 // import { motion, AnimatePresence } from "framer-motion";
 import {
@@ -10,68 +10,78 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { HiMinusSm } from "react-icons/hi";
+import { motion, AnimatePresence } from "framer-motion";
+import Paragraph from "./animatedText";
+import { LuMinus } from "react-icons/lu";
+import { LuPlus } from "react-icons/lu";
 
 const Faq = () => {
-  // const [activeIndex, setActiveIndex] = useState<number | null>(0);
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
 
-  // const accData = [
-  //   {
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
-  //   },
-  //   {
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
-  //   },
-  //   {
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
-  //   },
-  //   {
-  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
-  //   },
-  // ];
+  const accData = [
+    {
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
+    },
+    {
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.Aspernatur iste laudantium cum ipsam distinctio amet eaque in velit necessitatibus repellendus. Repellendus, porro molestias dicta deleniti ratione rerum? Similique, hic quo!",
+    },
+  ];
 
   return (
-    <div className="bg-[url('/bg/bg4.webp')] bg-center bg-cover flex flex-col gap-16 pl-12 py-20">
+    <div className="bg-[url('/bg/bg4.webp')] bg-center bg-cover flex flex-col gap-16 md:pl-12 py-20 max-sm:p-2 max-sm:pb-20">
       <h1 className="md:text-7xl text-5xl font-gilroyBold text-primaryText">
-        <AnimatedPara gap={1} words={"Frequently Asked Questions"} />
+        <Paragraph paragraph={"Frequently Asked Questions"} />
       </h1>
       <div className="flex justify-between gap-28">
-        {/* <div className="flex flex-col gap-8 max-w-[40%]">
+        <div className="flex flex-col gap-8 md:max-w-[40%]">
           {accData.map((e, i) => (
             <motion.div
+              layoutId={`faq-${i}`}
               key={i}
-              transition={{ duration: 0.5 }}
+              layout
+              transition={{
+                opacity: { ease: "linear" },
+                layout: { duration: 0.3 },
+              }}
               style={{
                 borderColor: activeIndex !== i ? "#3989FF" : "white",
+                height: activeIndex === i ? "auto" : "4rem",
               }}
-              className="border-2 pl-4 pr-0 flex gap-5 justify-between w-full"
+              className="border-2 pl-4 pr-0 flex gap-5 justify-between w-full overflow-hidden"
             >
-              <div className="pt-4 font-gilroyMedium text-lg">0{i + 1}</div>
-              <div>
-                <div className="py-4 font-gilroyMedium text-lg text-left">
+              <div className="pt-4 font-gilroyMedium text-lg text-primaryText">
+                0{i + 1}
+              </div>
+              <div className="flex-1">
+                <div className="py-4 font-gilroyMedium md:text-lg text-left text-primaryText max-sm:leading-none">
                   {e.title}
                 </div>
-                <AnimatePresence initial={false}>
-                  {activeIndex === i && (
-                    <motion.div
-                      key={i}
-                      initial={{ opacity: 0, height: 0 }}
-                      animate={{ opacity: 1, height: "auto" }}
-                      exit={{ opacity: 0, height: 0 }}
-                      transition={{ duration: 0.5 }}
-                      className="text-white py-4 overflow-hidden"
-                    >
-                      {e.description}
-                    </motion.div>
-                  )}
-                </AnimatePresence>
+                {
+                  <motion.div
+                    key={i}
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="text-white py-4 overflow-hidden"
+                  >
+                    {e.description}
+                  </motion.div>
+                }
               </div>
               <div
                 onClick={() => {
@@ -83,12 +93,12 @@ const Faq = () => {
                 }}
                 className="p-2 px-5 max-h-16 h-full flex justify-center items-center bg-red-50 text-3xl cursor-pointer"
               >
-                {activeIndex === i ? "-" : "+"}
+                {activeIndex === i ? <LuMinus /> : <LuPlus />}
               </div>
             </motion.div>
           ))}
-        </div> */}
-        <Accordion type="single" collapsible className="w-full text-white">
+        </div>
+        {/* <Accordion type="single" collapsible className="w-full text-white">
           <AccordionItem value="item-1">
             <AccordionTrigger>
               <div className="flex gap-3 px-4 w-full justify-between  ">
@@ -102,7 +112,7 @@ const Faq = () => {
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
-        </Accordion>
+        </Accordion> */}
         <Image
           className="w-1/2 max-sm:hidden"
           src="/image4 copy.png"
